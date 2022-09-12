d5a2bdc89a80c2f11f6c2a4151edc76f3aa81933
vinayreddy244 <83955648+vinayreddy244@users.noreply.github.com>
2022-09-12 07:17:33 +0530

    import java.util.Scanner; class Sample {     public static void main(String args[])     {         Scanner s=new Scanner(System.in);         int n1=s.nextInt();         int n2=s.nextInt();         int product=n1*n2;         System.out.println(product);     } }

---
+++ Multiplication.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+class Sample
+{
+    public static void main(String args[])
+    {
+        Scanner s=new Scanner(System.in);
+        int n1=s.nextInt();
+        int n2=s.nextInt();
+        int product=n1*n2;
+        System.out.println(product);
+    }
+}
