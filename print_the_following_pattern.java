4de2062894787295035f036b2c57e5a8fc5d80ba
vinayreddy244 <83955648+vinayreddy244@users.noreply.github.com>
2022-09-14 06:00:55 +0530

    import java.util.Scanner; class Sample {     public static void main(String args[])     {         Scanner s=new Scanner(System.in);         int n=s.nextInt();         for(int i=1;i<=n;i++)         {             for(int j=1;j<=n;j++)             {                 if(i==j||i+j==n+1)                 {                 System.out.print("x");                 }                 else                 {                     System.out.print(0);                 }             }             System.out.println();
            }
        }
    }

--- print_the_following_pattern.java
+++ print_the_following_pattern.java
@@ -1,24 +1,25 @@
 import java.util.Scanner;
-class Pattern
+class Sample
 {
-    public static void main(String[] args)
+    public static void main(String args[])
     {
         Scanner s=new Scanner(System.in);
         int n=s.nextInt();
-        for (int i =1; i<=n; i++)
+        for(int i=1;i<=n;i++)
         {
-            for (int j =1; j <= n; j++)
+            for(int j=1;j<=n;j++)
             {
-                if(i==j)
+                if(i==j||i+j==n+1)
                 {
-                System.out.print(0);
+                System.out.print("x");
                 }
                 else
                 {
-                    System.out.print("x");
+                    System.out.print(0);
                 }
             }
             System.out.println();
+            
         }
     }
 }
