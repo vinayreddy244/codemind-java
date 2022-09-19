bb9471b571fdeafbcd7eb14aaf1e682a9a9efdd1
vinayreddy244 <83955648+vinayreddy244@users.noreply.github.com>
2022-09-19 18:10:58 +0530

    import java.util.Scanner; class Sample {     public static void main(String args[])     {         Scanner s=new Scanner(System.in);         int n=s.nextInt();         int arr[]=new int[n];         for(int i=0;i<n;i++)         {             arr[i]=s.nextInt();         }             for(int i=n-1;i>=0;i--)             {                 if(arr[i]%2==0)                 {                     System.out.println(i);                     break;                 }             }     } }

---
+++ Last_even_element_index.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+class Sample
+{
+    public static void main(String args[])
+    {
+        Scanner s=new Scanner(System.in);
+        int n=s.nextInt();
+        int arr[]=new int[n];
+        for(int i=0;i<n;i++)
+        {
+            arr[i]=s.nextInt();
+        }
+            for(int i=n-1;i>=0;i--)
+            {
+                if(arr[i]%2==0)
+                {
+                    System.out.println(i);
+                    break;
+                }
+            }
+    }
+}
