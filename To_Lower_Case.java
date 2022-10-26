96751cfcc1d0319f6954215303e8246ebb08a23c
vinayreddy244 <83955648+vinayreddy244@users.noreply.github.com>
2022-10-26 06:34:08 +0530

    import java.util.Scanner; import java.util.*; class Sample {     public static void main(String args[])     {         Scanner s=new Scanner(System.in);         String sc=s.nextLine();         System.out.println(sc.toLowerCase());
        }
    }

---
+++ To_Lower_Case.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+import java.util.*;
+class Sample
+{
+    public static void main(String args[])
+    {
+        Scanner s=new Scanner(System.in);
+        String sc=s.nextLine();
+        System.out.println(sc.toLowerCase());
+        
+    }
+}
